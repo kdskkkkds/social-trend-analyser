48a91df73621a2df5e6a81570506b0fd4f012fb1
kedar <abc@xyz.com>
2022-12-21 12:26:24 +0530

    deleted aws credentials from source code

--- dags/3_1_user_app.py
+++ dags/3_1_user_app.py
@@ -6,8 +6,8 @@ from datetime import datetime as dt
 
 spark = SparkSession.builder.appName('user').getOrCreate()
 
-spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAS5JWNKZNMNJ2NWEM")
-spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "******************")
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "**************************")
 spark._jsc.hadoopConfiguration().set("fs.s3a.impl","org.apache.hadoop.fs.s3a.S3AFileSystem")
 spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
 spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider","org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
